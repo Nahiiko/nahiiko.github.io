7131668c00ea317319b7c03b1555a992017dfad2
Nahiiko <72504666+Nahiiko@users.noreply.github.com>
2021-01-06 22:25:45 +0100

    d
    d

---
+++ index.js
@@ -0,0 +1,17 @@
+if (window.web3) {
+  // Then replace the old injected version by the latest build of Web3.js version 1.0.0
+  window.web3 = new Web3(window.web3.currentProvider);
+}
+
+function startSign(){
+    var msgHash=document.getElementById("message").value;
+    web3.eth.getAccounts()
+    .then(function(accounts){
+        var from = accounts[0];
+        web3.eth.personal.sign(msgHash, from) //asking the user to sign in order to verify his identity
+        .then((value) => {
+        signature=value;
+        console.log(signature); //log the signature to verify consistency with the signature the server gets (only dev)
+        });
+    });
+}
