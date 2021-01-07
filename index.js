9d777c61c78bcbb90a72ce9003bcf44e8525a6b7
Nahiiko <72504666+Nahiiko@users.noreply.github.com>
2021-01-07 10:35:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ if (window.web3) { //if Metamask injected some web3 code
 }
 
 async function startSign(){
+    await window.ethereum.enable(); //give the code access to your ETH address
     var msgHash=document.getElementById("message").value; //get text in textbox
     web3.eth.getAccounts() //get user accounts 
     .then(function(accounts){
         var from = accounts[0]; //get the first ETH account
-        await window.ethereum.enable(); //give the code access to your ETH address
         console.log(from); //Log it
         web3.eth.personal.sign(msgHash, from) //asking the user to sign in order to verify his identity
         .then((value) => {
